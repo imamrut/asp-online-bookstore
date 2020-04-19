4f5760591b109ec1c4673e125a88633a7dd9e0f0
amrutpuri <amrutpuri@localhost>
2020-04-19 18:21:50 +0530

    Initial commit. Created Spring Boot project and configured application properties file.

---
+++ onlinebookstore/src/main/java/in/asp/onlinebookstore/AspOnlineBookStoreApplication.java
@@ -0,0 +1,13 @@
+package in.asp.onlinebookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AspOnlineBookStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AspOnlineBookStoreApplication.class, args);
+	}
+
+}
---
+++ onlinebookstore/src/test/java/in/asp/onlinebookstore/AspOnlineBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package in.asp.onlinebookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AspOnlineBookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
